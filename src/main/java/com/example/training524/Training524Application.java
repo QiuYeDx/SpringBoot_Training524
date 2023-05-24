a53105e1ba2c035cd4ea6fd92fd5e08d97a23b1f
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2023-05-24 11:54:18 +0800

    v0.1.0

---
+++ src/main/java/com/example/training524/Training524Application.java
@@ -0,0 +1,13 @@
+package com.example.training524;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Training524Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Training524Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/training524/Training524ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training524;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Training524ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
